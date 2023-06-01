a5191cb6f9bd314adcc6bc8cd50d817b7b588dcb
mvuqar1 <vuqar.5100@mail.ru>
2023-06-01 19:57:25 +0400

    scroll hover

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,78 @@
-import React from 'react'
-import logo from "./img/logo.png"
-import "./Navbar.css"
+import React, { useState, useEffect } from 'react';
+import logo from "./img/logo.png";
+import "./Navbar.css";
 
 export default function Navbar() {
-  window.onscroll=()=>{
-    let navbar = document.querySelector(".nav-container");
-    navbar.classList.toggle("toggle-scroll",window.scrollY>100)
-}
+  const [activeLink, setActiveLink] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
   
+    const aboutSection = document.getElementById("about");
+    const portfolioSection = document.getElementById("portfolio");
+    const contactsSection = document.getElementById("contacts");
+  
+    const aboutOffset = aboutSection.getBoundingClientRect().top + window.scrollY - 4 * 16;
+    const portfolioOffset = portfolioSection.getBoundingClientRect().top + window.scrollY - 1 * 16;
+    const contactsOffset = contactsSection.getBoundingClientRect().top + window.scrollY - 44 * 16;
+  
+    if (scrollPosition >= aboutOffset && scrollPosition < portfolioOffset) {
+      setActiveLink("about");
+    } else if (scrollPosition >= portfolioOffset && scrollPosition < contactsOffset) {
+      setActiveLink("portfolio");
+    } else if (scrollPosition >= contactsOffset) {
+      setActiveLink("contacts");
+    } else {
+      setActiveLink("");
+    }
+  };
+  const handleNavLinkClick = (id) => {
+    document.getElementById(id).scrollIntoView();
+    setActiveLink(id);
+  }
 
   return (
     <>
-    <div className="nav-container">
+      <div className={`nav-container ${activeLink ? 'toggle-scroll' : ''}`}>
         <div className="nav-left">
-            <img src={logo} alt=""/>
+          <img src={logo} alt="" />
         </div>
         <div className="nav-right">
-            <ul className='d-flex'>
-              <li>
-              <p onClick={() => document.getElementById("about").scrollIntoView()}>resume</p>
-              </li>
-              <li>
-                <p onClick={() => document.getElementById("portfolio").scrollIntoView()}>portfolio</p>
-              </li>
-             
-              <li>
-                <p onClick={() => document.getElementById("contacts").scrollIntoView()}>contact</p>
-              </li>
-            </ul>
-
+          <ul className='d-flex'>
+            <li>
+              <p
+                className={activeLink === "about" ? "active" : ""}
+                onClick={() => handleNavLinkClick("about")}
+              >
+                resume
+              </p>
+            </li>
+            <li>
+              <p
+                className={activeLink === "portfolio" ? "active" : ""}
+                onClick={() => handleNavLinkClick("portfolio")}
+              >
+                portfolio
+              </p>
+            </li>
+            <li>
+              <p
+                className={activeLink === "contacts" ? "active" : ""}
+                onClick={() => handleNavLinkClick("contacts")}
+              >
+                contact
+              </p>
+            </li>
+          </ul>
         </div>
-        
-    </div>
+      </div>
     </>
   )
 }

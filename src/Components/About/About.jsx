30a385bb843caa3877252414d3f28f9df6908b12
mvuqar1 <vuqar.5100@mail.ru>
2023-06-04 18:20:20 +0400

    framer-motion

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import me from "./img/me.png"
 import "./About.css"
+import{motion} from "framer-motion"
+
+const textAnimation={
+    hidden:{
+        x:-100,
+        opacity:0
+    },
+    visible:custom=>({
+        x:0,
+        opacity:1,
+        transition:{delay:custom * 0.2}
+
+    })
+}
 
 export default function About() {
     return (
-        <div id='about' className="about-container">
+        <motion.div 
+        initial="hidden"
+        whileInView="visible"
+        id='about' 
+        className="about-container">
             <div className="about-left">
-                <img src={me} alt='Vuqar' />
+                <motion.img variants={textAnimation} custom="3" src={me} alt='Vuqar' />
             </div>
             <div className="about-right">
                 <div className="about-right-items">
                     <div className="about-item me">
-                        <h1><span>A</span>bout me</h1>
+                        <motion.h1 variants={textAnimation} custom="1"><span>A</span>bout me</motion.h1>
                     </div>
                     <div className="about-item front">
-                        <h3>Junior Front-end Developer</h3>
+                        <motion.h3 variants={textAnimation} custom="2">Junior Front-end Developer</motion.h3>
                     </div>
                     <div className="about-item resume">
-                        <p>Hello, my name is Vugar, and I am a frontend developer with experience in HTML, CSS, JavaScript, React, Redux, Node.js, Express, and MySQL. I constantly strive to develop my skills to stay up-to-date with the latest trends and technologies in web development.
+                        <motion.p variants={textAnimation} custom="3">Hello, my name is Vugar, and I am a frontend developer with experience in HTML, CSS, JavaScript, React, Redux, Node.js, Express, and MySQL. I constantly strive to develop my skills to stay up-to-date with the latest trends and technologies in web development.
                             I specialize in creating beautiful and user-friendly web applications and enjoy working on both individual and team projects. My goal is always to provide high-quality work that exceeds expectations.
-                            In my free time, I watch web development video tutorials to improve my skills and knowledge.</p>
+                            In my free time, I watch web development video tutorials to improve my skills and knowledge.</motion.p>
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,18 +1,45 @@
 import React, { useContext } from 'react';
 import {PortfolioContext} from "../../Data/PortfolioData"
 import "./Portfolio.css";
+import{motion} from "framer-motion"
+
+
+const textAnimation={
+    hidden:{
+        y:100,
+        opacity:0
+    },
+    visible:custom=>({
+        y:0,
+        opacity:1,
+        transition:{delay:custom * 0.2}
+
+    })
+}
+
 
 export default function Portfolio() {
     const {initialState}=useContext(PortfolioContext)
     return (
         <>
-            <div id='portfolio' className="containet-portfolio">
+            <motion.div 
+            initial="hidden"
+            whileInView="visible"
+            viewport={{amount:0.1}}
+            id='portfolio' 
+            className="containet-portfolio"
+            >
                 <div className="portfolio-title">
                     <h2>portfolio</h2>
                 </div>
                 <div className="portfolio-items">
-                    {initialState.map((item) => (
-                        <div key={item.id} className="portfolio-item">
+                    {initialState.map((item,index) => (
+                        <motion.div
+                        variants={textAnimation}
+                        custom={index + 1} 
+
+                        key={item.id} 
+                        className="portfolio-item">
                             <div className="portfolio-sayt">
                                 <h4>{item.title}</h4>
                             </div>
@@ -23,10 +50,10 @@ export default function Portfolio() {
                                 <div className="portfolio-netlify btn"><p><a target='_blank' rel='noopener noreferrer' href={item.liveLink}>View Project</a></p></div>
                                 <div className="portfolio-github btn"><p><a target='_blank' rel='noopener noreferrer' href={item.gitLink}>View Github</a></p></div>
                             </div>
-                        </div>
+                        </motion.div>
                     ))}
                 </div>
-            </div>
+            </motion.div>
         </>
     );
     

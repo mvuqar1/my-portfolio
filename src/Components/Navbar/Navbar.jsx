8754a25fab58ea8db0f19212fe352bb05ef6dd4d
mvuqar1 <vuqar.5100@mail.ru>
2023-06-07 03:04:22 +0400

    menu bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import logo from "./img/logo.png";
+import close from "./img/close.png"
+import menu from "./img/menu.png"
 import "./Navbar.css";
+
 
 export default function Navbar() {
   const [activeToggle, setToggle] = useState(false);
   const [activeLink, setActiveLink] = useState("");
+  const[menuBar,SetMenuBar]=useState(false)
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -26,13 +30,12 @@ export default function Navbar() {
     const portfolioOffset = portfolioSection.getBoundingClientRect().top + window.scrollY - 5 * 16;
     const contactsOffset = contactsSection.getBoundingClientRect().top + window.scrollY - 44 * 16;
 
-// console.log(scrollPosition)
-    if(scrollPosition===0){
+    if (scrollPosition === 0) {
       setToggle(false);
     } else if (scrollPosition >= aboutOffset && scrollPosition < skillsOffset) {
       setActiveLink("about");
       setToggle(true)
-    }else if (scrollPosition >= skillsOffset && scrollPosition < portfolioOffset) {
+    } else if (scrollPosition >= skillsOffset && scrollPosition < portfolioOffset) {
       setActiveLink("skills");
       setToggle(true)
     } else if (scrollPosition >= portfolioOffset && scrollPosition < contactsOffset) {
@@ -55,7 +58,7 @@ export default function Navbar() {
         top: offset,
         behavior: "smooth"
       });
-    }else if (id === "portfolio") {
+    } else if (id === "portfolio") {
       const targetElement = document.getElementById(id);
       const offset = targetElement.offsetTop - 4 * 16;
 
@@ -77,40 +80,46 @@ export default function Navbar() {
           <img src={logo} alt="" />
         </div>
         <div className="nav-right">
-          <ul className='d-flex'>
-            <li>
-              <p
-                className={activeLink === "about" ? "active" : ""}
-                onClick={() => handleNavLinkClick("about")}
-              >
-                resume
-              </p>
-            </li>
-            <li>
-              <p
-                className={activeLink === "skills" ? "active" : ""}
-                onClick={() => handleNavLinkClick("skills")}
-              >
-                skills
-              </p>
-            </li>
-            <li>
-              <p
-                className={activeLink === "portfolio" ? "active" : ""}
-                onClick={() => handleNavLinkClick("portfolio")}
-              >
-                portfolio
-              </p>
-            </li>
-            <li>
-              <p
-                className={activeLink === "contacts" ? "active" : ""}
-                onClick={() => handleNavLinkClick("contacts")}
-              >
-                contact
-              </p>
-            </li>
-          </ul>
+          <div className="nav-list" style={{ right: menuBar ? '0' : '-100%' }}>
+            <ul className='d-flex'>
+              <li>
+                <p
+                  className={activeLink === "about" ? "active" : ""}
+                  onClick={() => handleNavLinkClick("about")}
+                >
+                  resume
+                </p>
+              </li>
+              <li>
+                <p
+                  className={activeLink === "skills" ? "active" : ""}
+                  onClick={() => handleNavLinkClick("skills")}
+                >
+                  skills
+                </p>
+              </li>
+              <li>
+                <p
+                  className={activeLink === "portfolio" ? "active" : ""}
+                  onClick={() => handleNavLinkClick("portfolio")}
+                >
+                  portfolio
+                </p>
+              </li>
+              <li>
+                <p
+                  className={activeLink === "contacts" ? "active" : ""}
+                  onClick={() => handleNavLinkClick("contacts")}
+                >
+                  contact
+                </p>
+              </li>
+
+            </ul>
+            <div className="nav-close"><img src={close} alt="close" onClick={() => SetMenuBar(!menuBar)} />
+            </div>
+            </div>
+            <div className="nav-menu"><img src={menu} alt="menu" onClick={() => SetMenuBar(!menuBar)} /></div>
         </div>
       </div>
     </>

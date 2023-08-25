a7f0ec2aee9bfd28191abc9a6d4b75e08a483d94
mvuqar1 <vuqar.5100@mail.ru>
2023-08-25 19:54:59 +0400

    auto scroll

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -22,18 +22,39 @@ const textAnimation = {
 export default function Slider() {
   const [slide, setSlide] = useState(0);
   const len = imgData.length;
+  const [autoSlide, setAutoSlide] = useState(true); // Автоматическая прокрутка по умолчанию
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (slide === -(len - 1) * 100) {
-        setSlide(0);
-      } else {
-        setSlide(prev => prev - 100);
-      }
-    }, 3000); 
+    let interval;
+
+    if (autoSlide) {
+      interval = setInterval(() => {
+        if (slide === -(len - 1) * 100) {
+          setSlide(0);
+        } else {
+          setSlide(prev => prev - 100);
+        }
+      }, 3000); // Интервал в миллисекундах (здесь установлено 3 секунды)
+    }
 
     return () => clearInterval(interval);
-  }, [slide, len]);
+  }, [slide, len, autoSlide]);
+
+  const leftButton = () => {
+    if (slide === 0) {
+      setSlide(-(len - 1) * 100);
+    } else {
+      setSlide(prev => prev + 100);
+    }
+  }
+
+  const rightButton = () => {
+    if (slide === -(len - 1) * 100) {
+      setSlide(0);
+    } else {
+      setSlide(prev => prev - 100);
+    }
+  }
 
   return (
     <motion.div 
@@ -41,11 +62,14 @@ export default function Slider() {
       whileInView="visible"
       viewport={{ amount: 0.1 }}
       className='slider-container'>
-      <motion.button variants={textAnimation} custom={1}><img src={left} alt="left" /></motion.button>
+      <motion.button variants={textAnimation} custom={1} onClick={leftButton}><img src={left} alt="left" /></motion.button>
       <motion.div variants={textAnimation} custom={3} images={imgData} slide={slide}>
         <SliderItem images={imgData} slide={slide} />
       </motion.div>
-      <motion.button variants={textAnimation} custom={1}><img src={right} alt="right" /></motion.button>
+      <motion.button variants={textAnimation} custom={1} onClick={rightButton}><img src={right} alt="right" /></motion.button>
+      <button onClick={() => setAutoSlide(prev => !prev)}>
+        {autoSlide}
+      </button>
     </motion.div>
   );
 }

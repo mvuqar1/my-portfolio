2fb1d196c608e28c70cccb9b4c40528d1f27fc47
mvuqar1 <vuqar.5100@mail.ru>
2023-08-25 19:51:17 +0400

    auto scroll

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import SliderItem from './SliderItem'
-import imgData from '../../assets/imgData'
-import "./Slider.css"
-import left from "../../assets/img/left.png"
-import right from "../../assets/img/right.png"
+import React, { useState, useEffect } from 'react';
+import SliderItem from './SliderItem';
+import imgData from '../../assets/imgData';
+import "./Slider.css";
+import left from "../../assets/img/left.png";
+import right from "../../assets/img/right.png";
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const textAnimation = {
   hidden: {
@@ -16,43 +16,36 @@ const textAnimation = {
     y: 0,
     opacity: 1,
     transition: { delay: 0.5 }
-
   })
 }
 
-
 export default function Slider() {
+  const [slide, setSlide] = useState(0);
+  const len = imgData.length;
 
-  const [slide, SetSlide] = useState(0)
-  const len = imgData.length
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (slide === -(len - 1) * 100) {
+        setSlide(0);
+      } else {
+        setSlide(prev => prev - 100);
+      }
+    }, 3000); // Интервал в миллисекундах (здесь установлено 3 секунды)
 
-  const leftButton = () => {
-    if (slide === 0) {
-      SetSlide(-(len - 1) * 100)
-    } else {
-      SetSlide(prev => prev + 100)
-    }
-  }
-  const rightButton = () => {
-    if (slide === -(len - 1) * 100) {
-      SetSlide(0)
-    } else {
-      SetSlide(prev => prev - 100)
-    }
+    return () => clearInterval(interval);
+  }, [slide, len]);
 
-  }
   return (
     <motion.div 
-    initial="hidden"
-    whileInView="visible"
-    viewport={{ amount: 0.1 }}
-    className='slider-container'>
-      <motion.button variants={textAnimation} custom={1} onClick={(e) => leftButton()}><img src={left} alt="left" /></motion.button>
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.1 }}
+      className='slider-container'>
+      <motion.button variants={textAnimation} custom={1}><img src={left} alt="left" /></motion.button>
       <motion.div variants={textAnimation} custom={3} images={imgData} slide={slide}>
-  <SliderItem images={imgData} slide={slide} />
-</motion.div>
-      <motion.button variants={textAnimation} custom={1} onClick={(e) => rightButton()}><img src={right} alt="right" /></motion.button>
-
+        <SliderItem images={imgData} slide={slide} />
+      </motion.div>
+      <motion.button variants={textAnimation} custom={1}><img src={right} alt="right" /></motion.button>
     </motion.div>
-  )
+  );
 }
